639c71fad766c05e245e800e352f6f630f370d45
jlowe3 <38127241+jlowe3@users.noreply.github.com>
2018-10-30 20:39:08 -0700

    Add files via upload

---
+++ RandomRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity; 
+    }
+}
